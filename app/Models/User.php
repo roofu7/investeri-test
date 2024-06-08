490d0146dc932f97596130017c21d06aec82b527
Travik73 <32553674+Travik73@users.noreply.github.com>
2024-06-08 19:46:35 +0400

    Прописал связи в моделях, внешние ключи в миграциях.

--- app/Models/User.php
+++ app/Models/User.php
@@ -54,6 +54,10 @@ class User extends Authenticatable
     {
         return $this->hasManyThrough(CompanyContact::class, Company::class);
     }
+    public function companyInvestProjects(): HasManyThrough
+    {
+        return $this->hasManyThrough(CompanyInvestProject::class, Company::class);
+    }
     public function companyActualLocations(): HasManyThrough
     {
         return $this->hasManyThrough(CompanyActualLocation::class, Company::class);
